01a31f3c4d441c758d9cfd75a98623c74beac73b
Matthew Listor <mattlistor@gmail.com>
2019-04-20 02:01:18 +0000

    Automatically backed up by Learn

--- objects.js
+++ objects.js
@@ -1,12 +1,6 @@
-const playlist = { artist: "band", song "song" }
- 
-updateObjectWithKeyAndValue(recipe, 'chocolate', '1 cup')
+const playlist = {artist: "song"}
 
-function updateObjectWithObject(targetObject, updatesObject) {
-  return Object.assign({}, targetObject, updatesObject)
-}
-
-function updateObjectWithKeyAndValue(obj, key, value) {
- 
-  return Object.assign({}, obj, { [key]: value })
+function updatePlaylist(obj, artist,  song) {
+  obj.artist = song
+  return obj
 }
